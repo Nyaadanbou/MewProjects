dbf8a000f95c34a452b3d04c6ba054855a7edd8c
Akiranya <porridgen@gmail.com>
2021-08-04 09:15:36 +0800

    Ready to move to jitpack

---
+++ src/main/java/co/mcsky/moecore/MoeCore.java
@@ -0,0 +1,16 @@
+package co.mcsky.moecore;
+
+import me.lucko.helper.plugin.ExtendedJavaPlugin;
+
+public class MoeCore extends ExtendedJavaPlugin {
+
+    @Override
+    protected void enable() {
+
+    }
+
+    @Override
+    protected void disable() {
+
+    }
+}

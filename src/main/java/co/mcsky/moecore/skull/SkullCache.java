20c092cdddd84cd55fec29ebf50c78e2d740086e
Akiranya <porridgen@gmail.com>
2021-09-09 14:24:45 +0800

    Updates build.gradle

--- src/main/java/co/mcsky/moecore/skull/SkullCache.java
+++ src/main/java/co/mcsky/moecore/skull/SkullCache.java
@@ -1,7 +1,9 @@
 package co.mcsky.moecore.skull;
 
+import com.google.common.base.Preconditions;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import me.lucko.helper.Events;
-import me.lucko.helper.Schedulers;
 import me.lucko.helper.cooldown.Cooldown;
 import me.lucko.helper.promise.Promise;
 import me.lucko.helper.terminable.Terminable;
@@ -9,7 +11,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,7 +25,7 @@ public enum SkullCache implements Terminable {
     INSTANCE;
 
     // the skull cache
-    private final Map<UUID, ItemStack> cache;
+    private final Cache<UUID, ItemStack> cache;
 
     // all scheduled (async) tasks
     private final Set<Terminable> scheduledTasks;
@@ -30,22 +34,13 @@ public enum SkullCache implements Terminable {
     private final Set<UUID> fetching;
 
     // cooldown to fire the fetch complete event
-    private final Cooldown eventFireCooldown;
+    private final Cooldown eventThreshold;
 
     SkullCache() {
-        this.cache = new HashMap<>();
+        this.cache = CacheBuilder.newBuilder().expireAfterWrite(6, TimeUnit.HOURS).build();
         this.fetching = new HashSet<>();
         this.scheduledTasks = new HashSet<>();
-        this.eventFireCooldown = Cooldown.of(200, TimeUnit.MILLISECONDS);
-
-        // refresh the cache at certain interval
-        Schedulers.builder()
-                .async()
-                .afterAndEvery(30, TimeUnit.MINUTES)
-                .run(() -> {
-                    scheduledTasks.forEach(Terminable::closeSilently);
-                    cache.forEach((id, item) -> fetch(item, id));
-                });
+        this.eventThreshold = Cooldown.of(200, TimeUnit.MILLISECONDS);
     }
 
     /**
@@ -55,17 +50,18 @@ public enum SkullCache implements Terminable {
         scheduledTasks.forEach(Terminable::closeSilently);
         scheduledTasks.clear();
         fetching.clear();
-        cache.clear();
+        cache.invalidateAll();
     }
 
     /**
-     * Modifies the head to give it a texture of the given player's UUID. The {@code item} will leave unchanged if the
-     * texture has not been fetched yet when calling this method.
+     * Modifies the head to give it a texture of the given player's UUID. The
+     * {@code item} will leave unchanged if the texture has not been fetched yet
+     * when calling this method.
      *
-     * @param id   the UUID of the player
+     * @param uuid the UUID of the player
      * @param item the head to be modified
      */
-    public void itemWithUuid(ItemStack item, UUID id) {
+    public void itemWithUuid(ItemStack item, UUID uuid) {
         // Gets a copy of item meta of the item
         ItemMeta itemMeta = item.getItemMeta();
 
@@ -74,47 +70,47 @@ public enum SkullCache implements Terminable {
             return;
         }
 
-        if (cache.containsKey(id)) {
+        if (cache.getIfPresent(uuid) != null) {
             // if cached, updates the skull's texture
 
-            mutateProfile(item, id);
+            mutateProfile(item, uuid);
         } else {
             // schedules a task to fetch the skull texture
 
-            fetch(item, id);
+            fetch(item, uuid);
         }
     }
 
     /**
      * Schedules a task to fetch the skull texture of given player's UUID.
      * <p>
-     * The texture to be fetched will be put into the cache ONLY when the task completes so that subsequent "get" calls
-     * on the cache will return the fetched textures.
+     * The texture to be fetched will be put into the cache ONLY when the task
+     * completes so that subsequent "get" calls on the cache will return the
+     * fetched textures.
      *
-     * @param id the UUID of the skull texture
+     * @param uuid the UUID of the skull texture
      */
-    private void fetch(ItemStack item, UUID id) {
-        if (!fetching.contains(id)) {
-            // schedule the fetch task iff the id is not being fetched right now
+    private void fetch(ItemStack item, UUID uuid) {
+        if (!fetching.contains(uuid)) {
+            // schedule the fetch task iff the uuid is not being fetched right now
 
-            // mark this id is being fetched
-            fetching.add(id);
+            // mark this uuid is being fetched
+            fetching.add(uuid);
 
             Promise<Void> fetchTask = Promise.start()
-                    .thenApplyAsync(n -> SkullCreator.itemWithUuid(item, id))
+                    .thenApplyAsync(n -> SkullCreator.itemWithUuid(item, uuid))
                     .thenAcceptSync(fetched -> {
-                        cache.put(id, fetched);
-                        mutateProfile(item, id);
+                        cache.put(uuid, fetched);
+                        mutateProfile(item, uuid);
+
+                        // fetched, un-mark the uuid
+                        fetching.remove(uuid);
 
                         // call event so that GUIs containing skull items
-                        // may be able to refresh the skull textures automatically
-                        // if they listen to this event
-                        if (eventFireCooldown.test()) {
+                        // may refresh the skull textures as being fetched
+                        if (fetching.isEmpty() || eventThreshold.test()) {
                             Events.call(new SkinFetchCompleteEvent());
                         }
-
-                        // fetched, un-mark the id
-                        fetching.remove(id);
                     });
 
             scheduledTasks.add(fetchTask);
@@ -125,12 +121,16 @@ public enum SkullCache implements Terminable {
      * Modifies the head to have the texture obtained from the cache.
      *
      * @param origin the head to be modified
-     * @param id     the player's UUID
+     * @param uuid   the player's UUID
      */
-    private void mutateProfile(ItemStack origin, UUID id) {
+    private void mutateProfile(ItemStack origin, UUID uuid) {
+        final ItemStack nullable = cache.getIfPresent(uuid);
+        Preconditions.checkNotNull(nullable); // this should never be null otherwise it's a fatal error
+
         SkullMeta originSkullMeta = (SkullMeta) origin.getItemMeta();
-        SkullMeta cachedSkullMeta = (SkullMeta) cache.get(id).getItemMeta();
-        // only set the player profile to avoid unexpected lore duplicate
+        SkullMeta cachedSkullMeta = (SkullMeta) nullable.getItemMeta();
+
+        // only set the player profile to avoid unexpected duplicated lore
         originSkullMeta.setPlayerProfile(cachedSkullMeta.getPlayerProfile());
         origin.setItemMeta(originSkullMeta);
     }

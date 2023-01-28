44b9081eee523ad9f315c4192ad35982a02ecf43
Akiranya <porridgen@gmail.com>
2023-01-29 03:26:17 +0800

    Fix NPE

--- src/main/java/cc/mewcraft/mewcore/item/api/PluginItemRegistry.java
+++ src/main/java/cc/mewcraft/mewcore/item/api/PluginItemRegistry.java
@@ -2,6 +2,7 @@ package cc.mewcraft.mewcore.item.api;
 
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,10 +24,20 @@ public final class PluginItemRegistry {
         this.constructors = new HashMap<>();
     }
 
+    /**
+     * Initialize the plugin item registry.
+     *
+     * @param parent the parent plugin
+     */
     public static void init(final Plugin parent) {
         INSTANCE = new PluginItemRegistry(parent);
     }
 
+    /**
+     * Get the singleton instance of this class.
+     *
+     * @return the singleton instance of this class
+     */
     public static PluginItemRegistry get() {
         if (INSTANCE == null)
             throw new IllegalStateException("Instance is not initialized yet");
@@ -45,7 +56,15 @@ public final class PluginItemRegistry {
         constructors.clear();
     }
 
-    public @NotNull ItemStack refreshItemStack(@NotNull ItemStack item) {
+    /**
+     * Update the given item stack to the latest version.
+     *
+     * @param item the item stack to refresh
+     *
+     * @return a new item stack if it's of plugin item, or the original item stack if it's not of plugin item
+     */
+    @Contract("null -> null; !null -> !null")
+    public ItemStack refreshItemStack(ItemStack item) {
         PluginItem<?> pluginItem = fromItemStackNullable(item);
         if (pluginItem != null) {
             ItemStack itemStack = pluginItem.createItemStack();
@@ -56,11 +75,20 @@ public final class PluginItemRegistry {
         return item;
     }
 
+    /**
+     * Get the plugin item derived from the given item stack.
+     *
+     * @param item the item stack to derive
+     *
+     * @return the plugin item derived from the given item stack
+     */
     public @Nullable PluginItem<?> fromItemStackNullable(@Nullable ItemStack item) {
-        if (item == null) return null;
+        if (item == null)
+            return null;
         for (Map.Entry<String, Supplier<PluginItem<?>>> entry : constructors.entrySet()) {
             PluginItem<?> pluginItem = entry.getValue().get();
-            if (!pluginItem.available()) return null;
+            if (!pluginItem.available())
+                return null;
             if (pluginItem.belongs(item)) {
                 String plugin = Objects.requireNonNull(entry.getKey()).toLowerCase(Locale.ROOT);
                 String itemId = Objects.requireNonNull(pluginItem.toItemId(item)).toLowerCase(Locale.ROOT);
@@ -73,13 +101,23 @@ public final class PluginItemRegistry {
         return null;
     }
 
+    /**
+     * Get the plugin item derived from the plugin id and item id.
+     *
+     * @param plugin the plugin id
+     * @param itemId the item id
+     *
+     * @return the plugin item derived from the plugin id and item id
+     */
     public @Nullable PluginItem<?> fromReferenceNullable(@Nullable String plugin, @Nullable String itemId) {
-        if (plugin == null || itemId == null) return null;
+        if (plugin == null || itemId == null)
+            return null;
         plugin = plugin.toLowerCase();
         itemId = itemId.toLowerCase();
         if (constructors.containsKey(plugin)) {
             PluginItem<?> pluginItem = constructors.get(plugin).get();
-            if (!pluginItem.available()) return null;
+            if (!pluginItem.available())
+                return null;
             pluginItem.setPlugin(plugin);
             pluginItem.setItemId(itemId);
             pluginItem.onConstruct();
@@ -90,40 +128,46 @@ public final class PluginItemRegistry {
         return null;
     }
 
+    /**
+     * Get the plugin item from the given item reference.
+     *
+     * @param reference item reference
+     *
+     * @return the plugin item from the given reference.
+     */
     public @Nullable PluginItem<?> fromReferenceNullable(@Nullable String reference) {
-        if (!isPluginItemId(reference)) {
+        if (!parseble(reference)) {
             parent.getLogger().severe("The format of plugin item ID '" + reference + "' is not correct. Correct format: {plugin}:{itemId}");
             return null;
         }
-        String[] split = toPluginItemId(reference);
-        return fromReference(split[0], split[1]);
+        String[] split = split(reference);
+        return fromReferenceNullable(split[0], split[1]);
     }
 
-    public @NotNull PluginItem<?> fromItemStack(@NotNull ItemStack item) {
-        PluginItem<?> pluginItem = fromItemStackNullable(item);
-        if (pluginItem == null) throw new NullPointerException();
-        return pluginItem;
+    public @NotNull PluginItem<?> fromItemStack(@NotNull ItemStack item) throws NullPointerException {
+        return Objects.requireNonNull(fromItemStackNullable(item));
     }
 
-    public @NotNull PluginItem<?> fromReference(@NotNull String plugin, @NotNull String itemId) {
+    public @NotNull PluginItem<?> fromReference(@NotNull String plugin, @NotNull String itemId) throws NullPointerException {
         return Objects.requireNonNull(fromReferenceNullable(plugin, itemId));
     }
 
-    public @NotNull PluginItem<?> fromReference(@NotNull String reference) {
+    public @NotNull PluginItem<?> fromReference(@NotNull String reference) throws NullPointerException {
         return Objects.requireNonNull(fromReferenceNullable(reference));
     }
 
-    public @Nullable String toReference(@Nullable ItemStack item) {
+    public @Nullable String asReference(@Nullable ItemStack item) {
         PluginItem<?> pluginItem = fromItemStackNullable(item);
-        if (pluginItem == null) return null;
+        if (pluginItem == null)
+            return null;
         return pluginItem.getPlugin() + ":" + pluginItem.getItemId();
     }
 
-    public boolean isPluginItemId(@Nullable String reference) {
-        return reference != null && toPluginItemId(reference).length == 2;
+    private boolean parseble(@Nullable String reference) {
+        return reference != null && split(reference).length == 2;
     }
 
-    private @NotNull String[] toPluginItemId(@NotNull String reference) {
+    private @NotNull String[] split(@NotNull String reference) {
         return reference.split(":", 2);
     }
 

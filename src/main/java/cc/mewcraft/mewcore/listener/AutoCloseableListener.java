49151e5cb74e31714c959a0caad44b642a64b265
Akiranya <porridgen@gmail.com>
2023-02-18 21:19:35 +0800

    Add AutoCloseableListener.java

---
+++ src/main/java/cc/mewcraft/mewcore/listener/AutoCloseableListener.java
@@ -0,0 +1,13 @@
+package cc.mewcraft.mewcore.listener;
+
+import me.lucko.helper.terminable.Terminable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+
+public interface AutoCloseableListener extends Listener, Terminable {
+
+    @Override default void close() {
+        HandlerList.unregisterAll(this);
+    }
+
+}

04cc136188a97e1663e31a40f12d7008c2ac0c29
Akiranya <porridgen@gmail.com>
2023-05-09 09:23:41 +0800

    Fix annotation warnings from IDEA

---
+++ src/main/java/cc/mewcraft/mewcore/util/PDCUtils.java
@@ -0,0 +1,333 @@
+package cc.mewcraft.mewcore.util;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataAdapterContext;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataHolder;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
+
+public class PDCUtils {
+
+    public static final PersistentDataType<byte[], double[]> DOUBLE_ARRAY = new DoubleArray();
+    public static final PersistentDataType<byte[], String[]> STRING_ARRAY = new StringArray(StandardCharsets.UTF_8);
+    public static final PersistentDataType<byte[], UUID> UUID = new UUIDDataType();
+
+    public static @NotNull <Z> Optional<Z> get(@NotNull ItemStack holder, @NotNull PersistentDataType<?, Z> type, @NotNull NamespacedKey key) {
+        ItemMeta meta = holder.getItemMeta();
+        if (meta == null) return Optional.empty();
+
+        return get(meta, type, key);
+    }
+
+    public static @NotNull <Z> Optional<Z> get(@NotNull PersistentDataHolder holder, @NotNull PersistentDataType<?, Z> type, @NotNull NamespacedKey key) {
+        PersistentDataContainer container = holder.getPersistentDataContainer();
+        if (container.has(key, type)) {
+            return Optional.ofNullable(container.get(key, type));
+        }
+        return Optional.empty();
+    }
+
+    public static void set(@NotNull ItemStack holder, @NotNull NamespacedKey key, boolean value) {
+        set(holder, PersistentDataType.INTEGER, key, value ? 1 : 0);
+    }
+
+    public static void set(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key, boolean value) {
+        set(holder, PersistentDataType.INTEGER, key, value ? 1 : 0);
+    }
+
+    public static void set(@NotNull ItemStack holder, @NotNull NamespacedKey key, double value) {
+        set(holder, PersistentDataType.DOUBLE, key, value);
+    }
+
+    public static void set(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key, double value) {
+        set(holder, PersistentDataType.DOUBLE, key, value);
+    }
+
+    public static void set(@NotNull ItemStack holder, @NotNull NamespacedKey key, int value) {
+        set(holder, PersistentDataType.INTEGER, key, value);
+    }
+
+    public static void set(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key, int value) {
+        set(holder, PersistentDataType.INTEGER, key, value);
+    }
+
+    public static void set(@NotNull ItemStack holder, @NotNull NamespacedKey key, long value) {
+        set(holder, PersistentDataType.LONG, key, value);
+    }
+
+    public static void set(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key, long value) {
+        set(holder, PersistentDataType.LONG, key, value);
+    }
+
+    public static void set(@NotNull ItemStack holder, @NotNull NamespacedKey key, @Nullable String value) {
+        set(holder, PersistentDataType.STRING, key, value);
+    }
+
+    public static void set(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key, @Nullable String value) {
+        set(holder, PersistentDataType.STRING, key, value);
+    }
+
+    public static void set(@NotNull ItemStack holder, @NotNull NamespacedKey key, String[] value) {
+        set(holder, STRING_ARRAY, key, value);
+    }
+
+    public static void set(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key, String[] value) {
+        set(holder, STRING_ARRAY, key, value);
+    }
+
+    public static void set(@NotNull ItemStack holder, @NotNull NamespacedKey key, double[] value) {
+        set(holder, DOUBLE_ARRAY, key, value);
+    }
+
+    public static void set(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key, double[] value) {
+        set(holder, DOUBLE_ARRAY, key, value);
+    }
+
+    public static void set(@NotNull ItemStack holder, @NotNull NamespacedKey key, @Nullable UUID value) {
+        set(holder, UUID, key, value);
+    }
+
+    public static void set(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key, @Nullable UUID value) {
+        set(holder, UUID, key, value);
+    }
+
+    public static <T, Z> void set(
+        @NotNull ItemStack item,
+        @NotNull PersistentDataType<T, Z> dataType,
+        @NotNull NamespacedKey key,
+        @Nullable Z value
+    ) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return;
+
+        set(meta, dataType, key, value);
+        item.setItemMeta(meta);
+    }
+
+    public static <T, Z> void set(
+        @NotNull PersistentDataHolder holder,
+        @NotNull PersistentDataType<T, Z> dataType,
+        @NotNull NamespacedKey key,
+        @Nullable Z value
+    ) {
+        if (value == null) {
+            remove(holder, key);
+            return;
+        }
+
+        PersistentDataContainer container = holder.getPersistentDataContainer();
+        container.set(key, dataType, value);
+    }
+
+    public static void remove(@NotNull ItemStack holder, @NotNull NamespacedKey key) {
+        ItemMeta meta = holder.getItemMeta();
+        if (meta == null) return;
+
+        remove(meta, key);
+    }
+
+    public static void remove(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key) {
+        PersistentDataContainer container = holder.getPersistentDataContainer();
+        container.remove(key);
+
+        /*if (holder instanceof BlockState state) {
+            state.update();
+        }*/
+    }
+
+    public static @NotNull Optional<String> getString(@NotNull ItemStack holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.STRING, key);
+    }
+
+    public static @NotNull Optional<String> getString(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.STRING, key);
+    }
+
+    public static @NotNull Optional<String[]> getStringArray(@NotNull ItemStack holder, @NotNull NamespacedKey key) {
+        return get(holder, STRING_ARRAY, key);
+    }
+
+    public static @NotNull Optional<String[]> getStringArray(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key) {
+        return get(holder, STRING_ARRAY, key);
+    }
+
+    public static @NotNull Optional<double[]> getDoubleArray(@NotNull ItemStack holder, @NotNull NamespacedKey key) {
+        return get(holder, DOUBLE_ARRAY, key);
+    }
+
+    public static @NotNull Optional<double[]> getDoubleArray(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key) {
+        return get(holder, DOUBLE_ARRAY, key);
+    }
+
+    public static @NotNull Optional<Integer> getInt(@NotNull ItemStack holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.INTEGER, key);
+    }
+
+    public static @NotNull Optional<Integer> getInt(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.INTEGER, key);
+    }
+
+    public static @NotNull Optional<Long> getLong(@NotNull ItemStack holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.LONG, key);
+    }
+
+    public static @NotNull Optional<Long> getLong(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.LONG, key);
+    }
+
+    public static @NotNull Optional<Double> getDouble(@NotNull ItemStack holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.DOUBLE, key);
+    }
+
+    public static @NotNull Optional<Double> getDouble(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.DOUBLE, key);
+    }
+
+    public static @NotNull Optional<Boolean> getBoolean(@NotNull ItemStack holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.INTEGER, key).map(i -> i != 0);
+    }
+
+    public static @NotNull Optional<Boolean> getBoolean(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key) {
+        return get(holder, PersistentDataType.INTEGER, key).map(i -> i != 0);
+    }
+
+    public static @NotNull Optional<UUID> getUUID(@NotNull ItemStack holder, @NotNull NamespacedKey key) {
+        return get(holder, UUID, key);
+    }
+
+    public static @NotNull Optional<UUID> getUUID(@NotNull PersistentDataHolder holder, @NotNull NamespacedKey key) {
+        return get(holder, UUID, key);
+    }
+
+    public static class DoubleArray implements PersistentDataType<byte[], double[]> {
+
+        @Override
+        public @NotNull Class<byte[]> getPrimitiveType() {
+            return byte[].class;
+        }
+
+        @Override
+        public @NotNull Class<double[]> getComplexType() {
+            return double[].class;
+        }
+
+        @Override
+        public byte @NotNull [] toPrimitive(double[] complex, @NotNull PersistentDataAdapterContext context) {
+            ByteBuffer bb = ByteBuffer.allocate(complex.length * 8);
+            for (double d : complex) {
+                bb.putDouble(d);
+            }
+            return bb.array();
+        }
+
+        @Override
+        public double @NotNull [] fromPrimitive(byte @NotNull [] primitive, @NotNull PersistentDataAdapterContext context) {
+            ByteBuffer bb = ByteBuffer.wrap(primitive);
+            DoubleBuffer dbuf = bb.asDoubleBuffer(); // Make DoubleBuffer
+            double[] a = new double[dbuf.remaining()]; // Make an array of the correct size
+            dbuf.get(a);
+
+            return a;
+        }
+    }
+
+    public static class StringArray implements PersistentDataType<byte[], String[]> {
+
+        private final Charset charset;
+
+        public StringArray(Charset charset) {
+            this.charset = charset;
+        }
+
+        @Override
+        public @NotNull Class<byte[]> getPrimitiveType() {
+            return byte[].class;
+        }
+
+        @Override
+        public @NotNull Class<String[]> getComplexType() {
+            return String[].class;
+        }
+
+        @Override
+        public byte @NotNull [] toPrimitive(String[] strings, @NotNull PersistentDataAdapterContext itemTagAdapterContext) {
+            byte[][] allStringBytes = new byte[strings.length][];
+            int total = 0;
+            for (int i = 0; i < allStringBytes.length; i++) {
+                byte[] bytes = strings[i].getBytes(this.charset);
+                allStringBytes[i] = bytes;
+                total += bytes.length;
+            }
+
+            ByteBuffer buffer = ByteBuffer.allocate(total + allStringBytes.length * 4); // stores integers
+            for (byte[] bytes : allStringBytes) {
+                buffer.putInt(bytes.length);
+                buffer.put(bytes);
+            }
+
+            return buffer.array();
+        }
+
+        @Override
+        public @NotNull String @NotNull [] fromPrimitive(byte @NotNull [] bytes, @NotNull PersistentDataAdapterContext itemTagAdapterContext) {
+            ByteBuffer buffer = ByteBuffer.wrap(bytes);
+            ArrayList<String> list = new ArrayList<>();
+
+            while (buffer.remaining() > 0) {
+                if (buffer.remaining() < 4)
+                    break;
+                int stringLength = buffer.getInt();
+                if (buffer.remaining() < stringLength)
+                    break;
+
+                byte[] stringBytes = new byte[stringLength];
+                buffer.get(stringBytes);
+
+                list.add(new String(stringBytes, this.charset));
+            }
+
+            return list.toArray(new String[0]);
+        }
+    }
+
+    public static class UUIDDataType implements PersistentDataType<byte[], UUID> {
+
+        @Override
+        public @NotNull Class<byte[]> getPrimitiveType() {
+            return byte[].class;
+        }
+
+        @Override
+        public @NotNull Class<UUID> getComplexType() {
+            return UUID.class;
+        }
+
+        @Override
+        public byte @NotNull [] toPrimitive(UUID complex, @NotNull PersistentDataAdapterContext context) {
+            ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+            bb.putLong(complex.getMostSignificantBits());
+            bb.putLong(complex.getLeastSignificantBits());
+            return bb.array();
+        }
+
+        @Override
+        public @NotNull UUID fromPrimitive(byte @NotNull [] primitive, @NotNull PersistentDataAdapterContext context) {
+            ByteBuffer bb = ByteBuffer.wrap(primitive);
+            long firstLong = bb.getLong();
+            long secondLong = bb.getLong();
+            return new UUID(firstLong, secondLong);
+        }
+    }
+}

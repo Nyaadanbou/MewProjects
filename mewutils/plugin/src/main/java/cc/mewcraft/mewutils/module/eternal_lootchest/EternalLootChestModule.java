7fd10947ca6dafa37d13146d94faa89d3bd73a3e
Akiranya <porridgen@gmail.com>
2023-06-26 13:47:04 +0800

    feat(utils): allow creative players to destroy lootchest

--- mewutils/plugin/src/main/java/cc/mewcraft/mewutils/module/eternal_lootchest/EternalLootChestModule.java
+++ mewutils/plugin/src/main/java/cc/mewcraft/mewutils/module/eternal_lootchest/EternalLootChestModule.java
@@ -4,10 +4,13 @@ import cc.mewcraft.mewcore.listener.AutoCloseableListener;
 import cc.mewcraft.mewutils.api.MewPlugin;
 import cc.mewcraft.mewutils.api.module.ModuleBase;
 import com.google.inject.Inject;
+import org.bukkit.GameMode;
 import org.bukkit.block.Chest;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.loot.Lootable;
 
 public class EternalLootChestModule extends ModuleBase implements AutoCloseableListener {
     @Inject
@@ -20,12 +23,17 @@ public class EternalLootChestModule extends ModuleBase implements AutoCloseableL
         registerListener(this);
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onBreak(BlockBreakEvent event) {
-        if (event.getBlock().getState() instanceof Chest chest) {
-            if (chest.hasLootTable()) {
-                event.setCancelled(true);
-                getLang().of("msg_lootchest_cannot_be_destroyed").send(event.getPlayer());
+        if (event.getBlock().getState() instanceof Lootable lootable) {
+            if (lootable.hasLootTable()) {
+                Player player = event.getPlayer();
+                if (player.getGameMode() != GameMode.CREATIVE) {
+                    event.setCancelled(true);
+                    getLang().of("msg_lootchest_cannot_be_destroyed").send(player);
+                } else {
+                    getLang().of("msg_lootchest_destroyed_by_creative").send(player);
+                }
             }
         }
     }

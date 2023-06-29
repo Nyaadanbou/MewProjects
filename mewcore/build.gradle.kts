89f49eed675e1f3c74f687274376752a434272f1
Akiranya <porridgen@gmail.com>
2023-06-29 15:49:58 +0800

    chore

--- mewcore/build.gradle.kts
+++ mewcore/build.gradle.kts
@@ -14,33 +14,38 @@ description = "Common code of all Mewcraft plugins."
 
 // Reference: https://youtrack.jetbrains.com/issue/IDEA-276365
 /*configurations {
-    compileOnlyApi { isCanBeResolved = true }
-    implementation { isCanBeResolved = true }
+    compileOnly {
+        isTransitive = false
+    }
 }*/
 
 dependencies {
     // Shaded libs at compile path - these are essential for plugin development
     compileOnlyApi(libs.guice)
+    compileOnlyApi(libs.lang.bukkit)
     compileOnlyApi(libs.bundles.cmds) {
         exclude("net.kyori")
     }
-    compileOnlyApi(libs.lang.bukkit)
-    compileOnlyApi(libs.authlib) {
+
+    // Shaded libs at runtime path - these are optional
+    // Consumers are expected to declare these dependencies
+    // at [compile path] if they need these dependencies
+    runtimeOnly(libs.hikari)
+    runtimeOnly(libs.anvilgui)
+    runtimeOnly(libs.bundles.invui)
+    runtimeOnly(libs.configurate)
+    runtimeOnly(libs.simplixstorage)
+    runtimeOnly(libs.cronutils)
+    runtimeOnly(libs.authlib) {
         exclude("com.google.guava")
         exclude("com.google.code.gson")
         exclude("com.google.code.findbugs")
         exclude("org.apache.logging.log4j")
     }
 
-    // Shaded libs at runtime path - these are optional
-    // Consumers are expected to declare these dependencies
-    // at [compile path] if they need these dependencies
-    implementation(libs.hikari)
-    implementation(libs.anvilgui)
-    implementation(libs.bundles.invui)
-    implementation(libs.configurate)
-    implementation(libs.simplixstorage)
-    implementation(libs.cronutils)
+    // Libs provided by core (itself)
+    compileOnly(libs.authlib)
+    compileOnly(libs.configurate)
 
     // Plugin libs - these will be present as other plugins
     compileOnly(libs.helper)
@@ -52,27 +57,16 @@ dependencies {
     compileOnly(libs.luckperms)
     compileOnly(libs.vault) { isTransitive = false }
     compileOnly(libs.essentials) { isTransitive = false }
-    compileOnly(libs.towny)
-    compileOnly(libs.itemsadder)
-    compileOnly(libs.mythiclib)
-    compileOnly(libs.mmoitems)
-    compileOnly(libs.interactivebooks)
+    compileOnly(libs.towny) { isTransitive = false }
+    compileOnly(libs.itemsadder) { isTransitive = false }
+    compileOnly(libs.mythiclib) { isTransitive = false }
+    compileOnly(libs.mmoitems) { isTransitive = false }
+    compileOnly(libs.interactivebooks) { isTransitive = false }
     compileOnly(libs.brewery) { isTransitive = false }
     compileOnly(libs.papi) { isTransitive = false }
     compileOnly(libs.minipapi) { isTransitive = false }
 
-    testImplementation(libs.server.paper)
     testImplementation(libs.junit.jupiter)
+    testImplementation(libs.server.paper)
     testImplementation(libs.helper)
-}
-
-tasks {
-    shadowJar {
-        /*configurations = listOf(
-            project.configurations.implementation.get(),
-            project.configurations.compileOnlyApi.get()
-        )*/
-
-        // Paper Plugins have isolated classloaders so relocating classes is no longer needed
-    }
 }

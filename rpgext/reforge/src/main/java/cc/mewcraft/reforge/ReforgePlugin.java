439c3670be823c5fafea6f0f949c081faf3599bb
Akiranya <porridgen@gmail.com>
2023-07-01 01:28:57 +0800

    fix: reload not working

--- rpgext/reforge/src/main/java/cc/mewcraft/reforge/ReforgePlugin.java
+++ rpgext/reforge/src/main/java/cc/mewcraft/reforge/ReforgePlugin.java
@@ -1,14 +1,20 @@
 package cc.mewcraft.reforge;
 
 import cc.mewcraft.reforge.api.ReforgeProvider;
+import cc.mewcraft.reforge.command.PluginCommands;
 import cc.mewcraft.reforge.hook.MMOItemsReforge;
 import com.google.inject.AbstractModule;
+import com.google.inject.ConfigurationException;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.ProvisionException;
 import me.lucko.helper.plugin.ExtendedJavaPlugin;
 
 public class ReforgePlugin extends ExtendedJavaPlugin {
+    private PluginCommands pluginCommands;
+
     @Override protected void enable() {
+        // Save default config and reload it
         saveDefaultConfig();
         reloadConfig();
 
@@ -18,11 +24,20 @@ public class ReforgePlugin extends ExtendedJavaPlugin {
             }
         });
 
+        // Register reforge provider
         if (isPluginPresent("MMOItems")) {
             ReforgeProvider.register(injector.getInstance(MMOItemsReforge.class));
             getSLF4JLogger().info("Registered reforge provider: MMOItems");
         } else {
             getSLF4JLogger().error("Registered reforge provider: NONE");
+        }
+
+        // Register commands
+        try {
+            pluginCommands = injector.getInstance(PluginCommands.class);
+            pluginCommands.registerCommands();
+        } catch (ConfigurationException | ProvisionException e) {
+            getSLF4JLogger().error("Failed to register commands!", e);
         }
     }
 
--- rpgext/reforge/src/main/java/cc/mewcraft/reforge/command/PluginCommands.java
+++ rpgext/reforge/src/main/java/cc/mewcraft/reforge/command/PluginCommands.java
@@ -2,22 +2,18 @@ package cc.mewcraft.reforge.command;
 
 import cc.mewcraft.reforge.ReforgePlugin;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import com.google.inject.Singleton;
 
 @Singleton
 public class PluginCommands {
-    private final Injector injector;
     private final ReforgePlugin plugin;
     private final CommandRegistry registry;
 
     @Inject
     public PluginCommands(
-        Injector injector,
         ReforgePlugin plugin,
         CommandRegistry registry
     ) {
-        this.injector = injector;
         this.plugin = plugin;
         this.registry = registry;
     }
@@ -29,7 +25,8 @@ public class PluginCommands {
             .literal("reload")
             .permission("reforge.command.reload")
             .handler(ctx -> {
-                plugin.reloadConfig();
+                plugin.onDisable();
+                plugin.onEnable();
             }).build());
 
         // Register commands

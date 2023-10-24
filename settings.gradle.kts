3dec80997175f2acf288db903ae91e8f49e6b5dc
Akiranya <porridgen@gmail.com>
2023-10-24 21:13:00 +0800

    Let's make something big

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -102,6 +102,10 @@ include(":townyext:towny-link:plugin")
 include(":townyext:towny-origin")
 include(":townyext:towny-portal")
 
+include(":wakame:wakame-api")
+include(":wakame:wakame-nms")
+include(":wakame:wakame-plugin")
+
 include(":worldlink")
 
 include(":worldreset")

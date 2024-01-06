4e4d7af38699153fcf77fbc89c1626fd9f58e5ec
Akiranya <porridgen@gmail.com>
2024-01-06 15:53:55 +0800

    update Kotlin to 1.9.22

--- buildSrc/src/main/kotlin/cc.mewcraft.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/cc.mewcraft.kotlin-conventions.gradle.kts
@@ -44,15 +44,16 @@ kotlin {
                 // All kotlin dependencies are shipped with: https://github.com/GamerCoder215/KotlinMC
                 // So we can just declare all these kotlin dependencies as `compileOnly`.
                 compileOnly(kotlin("stdlib"))
-                compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.0")
-                compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-                compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.6.0")
+                compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
+                compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.2")
+                compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+                compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.6.2")
                 compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
                 compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.7.3") {
                     isTransitive = false
                 }
                 compileOnly("org.jetbrains.kotlinx:kotlinx-io-core:0.3.0")
-                compileOnly("org.jetbrains.kotlinx:atomicfu:0.22.0")
+                compileOnly("org.jetbrains.kotlinx:atomicfu:0.23.1")
             }
         }
         val test by getting {
